43e8d3e851d910366ef3421750cb2c81310f47e9
Mahmoud Ahmed <pythondeveloper6@gmail.com>
2026-01-08 19:25:35 +0200

    add admin customization for user & user profile

--- users/accounts/admin.py
+++ users/accounts/admin.py
@@ -1,8 +1,20 @@
-from multiprocessing.reduction import register
-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User, UserProfile
 
-admin.site.register(User)
-admin.site.register(UserProfile)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ["email", "username", "first_name", "last_name"]
+    list_filter = ["is_verified", "date_joined"]
+    search_fields = ["email", "first_name", "last_name", "username"]
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "created_at", "updated_at"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["user__email", "user__username", "bio"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
